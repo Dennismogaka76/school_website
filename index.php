b0e1faa8400502539039e41cd2da30cddbda332a
Dennis Mogaka <101080996+Dennismogaka76@users.noreply.github.com>
2022-09-20 10:42:04 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>St. Anthony's Boys High School-Kitale</title>
+	<link rel="shortcut icon" type="image/jpg" href="images/logo.jpg">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	
+</head> 
+<body>
+<header>
+
+	
+			<img class="logo" src="images/logo.jpg" width="150px">
+		
+		<h1>ST. ANTHONY'S BOYS HIGH SCHOOL-KITALE</h1>
+
+
+
+	
+	<nav>
+	  <ul>
+  	<li><a href="index.php">HOME</a></li>
+  	<li><a href=""> ABOUT</a>
+  	<ul>
+
+							<li><a href="location.php"target="_blank"> Location</a> </li>
+							<li><a href="history.php"target="_blank"> History</a></li>
+							<li style="height: 80px"><a href="motto.php"target="_blank">Motto,Mission & Vision</a></li>
+
+						</ul>
+</li>
+  	<li><a href="">ADMINISTRATION</a>
+  		<ul>                <li><a href="bog.php"target="_blank">B.O.G</a></li>
+  			                <li><a href="pta.php"target="_blank">P.T.A</a></li>
+							<li><a href="principal.php"target="_blank">Principal</a></li>
+							<li><a href="deputies.php"target="_blank">Deputy-principals</a></li>
+							<li><a href="studentsbody.php"target="_blank">Students Body</a></li>
+						</ul>
+  	</li>
+  	<li><a href="">ACADEMICS</a>
+  		<ul>
+							<li><a href="departments.php" target="_blank">Departments</a></li>
+							<li><a href="staff.php" target="_blank">Staff</a></li>
+							<li><a href="examinations.php" target="_blank">Examininations</a></li>
+
+
+
+						</ul>
+
+  	</li>
+
+  	<li><a href="">ACCOMODATION</a>
+  		<ul>
+  			<li><a href="dormitories.php" target="_blank">Dormitories</a></li>
+  		</ul>
+  	</li>
+  	<li><a href="">CO-CURRICULAR</a>
+<ul>
+							<li><a href="sports.php"target="_blank">Sports</a></li>
+							<li><a href="drama.php"target="_blank">Drama</a></li>
+
+						</li></ul>
+						</ul>
+<ul> </li><li><a href="">ALUMNI</a>
+<ul>
+							<li><a href="display.php"target="_blank">Alumni</a></li>
+							<li style="height: 80px;line-height: 40px;"><a href="login.php"target="_blank">Alumni registration and login</a></li>
+
+						</li></ul></ul></nav><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+	 
+
+
+</header>
+<main>
+<div class="slider">
+	<figure>
+		<div class="slide">
+			<img src="images/classroom.jpg">
+		</div>
+		<div class="slide">
+			<img src="images/results.jpg">
+		</div>
+		<div class="slide">
+			<img src="images/soccer.jpg">
+		</div>
+		<div class="slide">
+			<img src="images/assembly.jpg">
+		</div>
+		<div class="slide">
+			<img src="images/classphoto.jpg">
+		</div>
+	</figure>
+	
+	<video src="videos/schoolanthem.mp4" controls>
+
+				
+	</video>
+	<audio src="audios/schoolanthem.mp3" controls></audio>
+	<p style="font-size: 25px;color: white;">students singing the school anthem</p><br><br>
+</div>
+<article style="
+	margin-left: 7%;
+">
+	<section style="color:white;
+	margin-left: 30px;font-size:25px;width: 80%;text-align: center;">
+<h3 style="color:gold; ">Why choose St.Anthony's boys high school-kitale?</h3>
+<p>St.Anthonys boys high school has a unique and long standing reputation within the country.Our cultural diversity;academic ,creative and sporting prowess and wide ranging opportunities provided to broaden the mind,body and spirit are well established.We equip our students to develop into effective young adults and citizens</p>
+<p>we aim to ensure this thruogh parent patnership with parents,support staff and other stakeholders.The cohesive community enable our students to stimulated academically.Each student is encouraged to aim high and is supported to achieve personal examination success;opening the door to further and higher education and their chosen career path. </p>
+</section>
+</article>
+</main><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+<footer  style="font-size: 30px; color: white;text-align: center; border:3px solid blue">
+	<a name="contactus">CONTACT US</a><br><br>
+	<a href="www.facebook.com/st.anthony boy high school kitale" target="_blank" title="facebook"><img src="images/facebook.png" width="70px"></a>
+	<a href="www.instagram.com/st.anthony boy high school kitale" target="_blank" title="instagram"><img src="images/instagram.jpg"width="70px"></a>
+	<a href="www.twitter.com/st.anthony boy high school kitale" target="_blank" title="twitter"><img src="images/twitter.png"width="70px"></a>
+	<a href="tel:054-31006" title="call us"><img src="images/telephone.png"width="70px"></a>
+	<a href="mailto:stanthonyskitale@gmail.com" title="email us"><img src="images/email.png" width="70px"></a>
+	<a href="www.whatsapp.com/+254716418147" title="whatsapp us"><img src="images/whatsapp.png"width="70px"></a>
+	<br><br><br><br><br>
+	<form action="comments.php" method="POST">
+		<fieldset>
+			Name:<input type="text" name="name" maxlength="80"><br>
+			Email:<input type="email" name="email" value="@gmail.com" maxlength="60"><br>
+			Telephone Number:<input type="number" name="phonenumber" value="+2547" maxlength="13"><br>
+			Leave comment below:<br><textarea rows="20" cols="100" ></textarea><br>
+			<input type="submit" value="submit" name="submit">
+		</fieldset>
+	</form>
+</footer>	
+</body>
+</html>
